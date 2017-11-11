bfb8d62a4d8b446febd78d6a0eff08bf13b4bdd9
Varun Radhakrishnan <vradhak2@jhu.edu>
2017-11-11 17:30:14 -0500

    determines duration for playing one note at a time

--- miditest.py
+++ miditest.py
@@ -1,7 +1,22 @@
 import mido
 
 messages = mido.parse_all(open('Beethoven_Minuet_inG.mid', 'rb').read());
+currentNote = 0
+notes = []
+time = 0
 for message in messages :
-    print(message);
+    messageInformation = message.dict();
+    time += messageInformation['time']
 
-print (5 * 5);
+    type = messageInformation['type'];
+
+    if (type == 'note_on' and currentNote == 0) :
+        currentNote = messageInformation['note']
+        continue
+    
+    if (type == 'note_off' and messageInformation['note'] == currentNote) :
+        notes.append({currentNote : time})
+        currentNote = 0
+        time = 0
+        
+print(notes)
