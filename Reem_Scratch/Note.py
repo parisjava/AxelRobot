05ff237d58fbc499390b2d3da9c7410a6b267280
Reem Larabi <rlarabi3279@gmail.com>
2018-10-13 14:49:41 -0400

    added changes to Note, might be merge conflicts

--- Reem_Scratch/Note.py
+++ Reem_Scratch/Note.py
@@ -1,38 +1,20 @@
 import array as arr
 
-# class Note(object):
-#
-#     def __init__(self):
-#         self.note_name = ""
-#         self.dist_btwn = 0
-#         self.str_num = 0
-#
-#     def distance(self):
-#         return self.dist_btwn
-#
-#     def string_num(self):
-#         return self.str_num
-#
-#     def set_dist(self, x):
-#         self.dist_btwn = x
-#
-#     def set_string_num(self, x):
-#         self.str_num = x
-
-midpoint_list = []
+midpoint_list = [17.1, 15.9, 14.25, 13.25, 12.75, 11.7, 11, 10.35, 9.75, 9.2, 8.65, 8.1]
+accumulated_dist = []
 musical_notes = ['E', 'F', 'G', 'A', 'B', 'C', 'D', 'E']
 
-open_E = arr.array()
-open_A = arr.array()
-open_D = arr.array()
-open_G = arr.array()
-open_B = arr.array()
-open_e = arr.array()
+open_E = arr.array('i', [x for x in range(12)])
+open_A = arr.array('i', [x + 5 for x in range(12)])
+open_D = arr.array('i', [x + 10 for x in range(12)])
+open_G = arr.array('i', [x + 15 for x in range(12)])
+open_B = arr.array('i', [x + 19 for x in range(12)])
+open_e = arr.array('i', [x + 24 for x in range(12)])
 
-for i in range(0, 12):
-    open_E[i] = i
-    open_A[i] = i + 5
-    open_D[i] = i + 10
-    open_G[i] = i + 15
-    open_B[i] = i + 19
-    open_e[i] = i + 24
+print(open_E)
+print(open_A)
+print(open_D)
+print(open_G)
+print(open_B)
+print(open_e)
+
