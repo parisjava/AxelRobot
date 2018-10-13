7b682a6768994379e54b3717a1c83e3d7b5c3e17
Reem Larabi <rlarabi3279@gmail.com>
2018-10-13 14:15:39 -0400

    change Note.py and note_test.py

--- Reem_Scratch/Note.py
+++ Reem_Scratch/Note.py
@@ -1,35 +1,38 @@
 import array as arr
 
-class Note(object):
+# class Note(object):
+#
+#     def __init__(self):
+#         self.note_name = ""
+#         self.dist_btwn = 0
+#         self.str_num = 0
+#
+#     def distance(self):
+#         return self.dist_btwn
+#
+#     def string_num(self):
+#         return self.str_num
+#
+#     def set_dist(self, x):
+#         self.dist_btwn = x
+#
+#     def set_string_num(self, x):
+#         self.str_num = x
 
-    def __init__(self):
-        self.note_name = ""
-        self.dist_btwn = 0
-        self.str_num = 0
+midpoint_list = []
+musical_notes = ['E', 'F', 'G', 'A', 'B', 'C', 'D', 'E']
 
-    def distance(self):
-        return self.dist_btwn
+open_E = arr.array()
+open_A = arr.array()
+open_D = arr.array()
+open_G = arr.array()
+open_B = arr.array()
+open_e = arr.array()
 
-    def string_num(self):
-        return self.str_num
-
-    def set_dist(self, x):
-        self.dist_btwn = x
-
-    def set_string_num(self, x):
-        self.str_num = x
-
-
-class Strings(object):
-    midpoint_list = []
-    def populate_array():
-        musical_notes = ['E', 'F', 'G', 'A', 'B', 'C', 'D', 'E']
-        notes_list = []
-
-
-    open_E = arr.array()
-    open_A = arr.array()
-    open_D = arr.array()
-    open_G = arr.array()
-    open_B = arr.array()
-    open_e = arr.array()
+for i in range(0, 12):
+    open_E[i] = i
+    open_A[i] = i + 5
+    open_D[i] = i + 10
+    open_G[i] = i + 15
+    open_B[i] = i + 19
+    open_e[i] = i + 24
